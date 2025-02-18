ac2d2552cec6b176a5771ebe91b3d293635b2c34
snimmo <stephennimmo@gmail.com>
2025-02-18 10:24:02 -0600

    LegalEntityResourceV1Test.failCreateLegalEntityInvalidLegalEntityIdentifier works

--- src/main/java/com/osetrm/api/legalentity/LegalEntityV1.java
+++ src/main/java/com/osetrm/api/legalentity/LegalEntityV1.java
@@ -1,9 +1,12 @@
 package com.osetrm.api.legalentity;
 
 import jakarta.validation.constraints.NotEmpty;
+import org.hibernate.validator.constraints.Length;
 
 public record LegalEntityV1(
-        @NotEmpty(message = "{LegalEntity.legalEntityIdentifier.required}") String legalEntityIdentifier,
+        @NotEmpty(message = "{LegalEntity.legalEntityIdentifier.required}")
+        @Length(min = 20, max = 20, message = "{LegalEntity.legalEntityIdentifier.length}")
+        String legalEntityIdentifier,
         @NotEmpty(message = "{LegalEntity.legalName.required}") String legalName
 ) {
 }
--- src/test/java/com/osetrm/api/legalentity/LegalEntityResourceV1Test.java
+++ src/test/java/com/osetrm/api/legalentity/LegalEntityResourceV1Test.java
@@ -1,5 +1,7 @@
 package com.osetrm.api.legalentity;
 
+import com.osetrm.api.common.ErrorMessage;
+import com.osetrm.api.common.ErrorResponse;
 import io.quarkus.test.junit.QuarkusTest;
 import io.quarkus.test.security.TestSecurity;
 import io.restassured.http.ContentType;
@@ -25,6 +27,27 @@ class LegalEntityResourceV1Test {
                 .statusCode(Response.Status.CREATED.getStatusCode())
                 .extract().as(LegalEntityV1.class);
         assertThat(saved).isEqualTo(legalEntity);
+    }
+
+    @Test
+    @TestSecurity(user = "test", roles = {Role.LEGAL_ENTITY_WRITE})
+    void failCreateLegalEntityInvalidLegalEntityIdentifier() {
+        LegalEntityV1 legalEntity = new LegalEntityV1(
+                RandomStringUtils.insecure().nextAlphanumeric(10),
+                RandomStringUtils.insecure().nextAlphanumeric(20)
+        );
+        ErrorResponse errorResponse = given()
+                .contentType(ContentType.JSON)
+                .body(legalEntity)
+                .post("/api/v1/legal-entities")
+                .then()
+                .statusCode(Response.Status.BAD_REQUEST.getStatusCode())
+                .extract().as(ErrorResponse.class);
+        assertThat(errorResponse).isNotNull()
+                .extracting(ErrorResponse::errorId).isNull();
+        assertThat(errorResponse.errorMessages())
+                .extracting(ErrorMessage::message)
+                .contains("The Legal Entity's legal entity identifier (LEI) must be twenty characters");
     }
 
     private LegalEntityV1 random() {
